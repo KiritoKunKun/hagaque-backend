b3e25b0d85fde79ccbed20d2cf901ed4b9b401de
Pedro Henrique <pedro.ribeiro@meritt.com.br>
2021-07-27 22:28:42 -0300

    Creating delete category test

---
+++ src/modules/categories/services/deleteCategory/DeleteCategoryService.spec.ts
@@ -0,0 +1,39 @@
+import { CategoriesRepositoryInMemory } from '@modules/categories/infra/typeorm/repositories/inMemory/CategoriesRepositoryInMemory';
+import { CreateCategoryDTO } from '@modules/categories/types/CategoriesRepositoryTypes';
+import { CreateCategoryService } from '../createCategory/CreateCategoryService';
+import { ListCategoriesService } from '../listCategories/ListCategoriesService';
+import { DeleteCategoryService } from './DeleteCategoryService';
+
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+let createCategoryService: CreateCategoryService;
+let deleteCategoryService: DeleteCategoryService;
+let listCategoriesService: ListCategoriesService;
+
+describe('Delete Category', () => {
+	beforeEach(() => {
+		categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+		createCategoryService = new CreateCategoryService(
+			categoriesRepositoryInMemory
+		);
+		deleteCategoryService = new DeleteCategoryService(
+			categoriesRepositoryInMemory
+		);
+		listCategoriesService = new ListCategoriesService(
+			categoriesRepositoryInMemory
+		);
+	});
+
+	it('should be able to list all categories', async () => {
+		const category: CreateCategoryDTO = {
+			name: 'Category Test',
+		};
+
+		const createdCategory = await createCategoryService.execute(category);
+
+		await deleteCategoryService.execute({ id: createdCategory.id });
+
+		const categories = await listCategoriesService.execute();
+
+		expect(categories).not.toContain(createdCategory);
+	});
+});
